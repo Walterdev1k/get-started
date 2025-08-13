5db3656bdad922e2fd9afa14f3baa78de0a1b408
WALTER DOMICIANO DE SOUZA <walterdev1k81@hotmail.com>
2025-08-13 20:42:33 -0300

    Update AnotherClass.java

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -1,4 +1,15 @@
-package main.java;
-
-public class AnotherClass {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+       var scanner = new Scanner(System.in);
+       /*entende que está criando uma variavel, tipando ou instanciando */
+        System.out.println("Olá, informe seu nome");
+        //String name = scanner.next();
+        var name = scanner.next();
+        System.out.println("Informe a sua idade");
+        //int age = scanner.nextInt();
+        var age = scanner.nextInt();
+        System.out.printf("Ola %s sua idade é %s \n" , name, age);
+    }
 }
